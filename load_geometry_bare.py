c025c4fb21bd898b23321e12f6caac55c7f1bb0f
A-Seyi <67125095+A-Seyi@users.noreply.github.com>
2020-06-18 22:41:16 +0100

    Update load_geometry_bare.py (#2)

--- load_geometry_bare.py
+++ load_geometry_bare.py
@@ -27,6 +27,7 @@ def main() :
     element1.Zy = 0.0
     element1.Zz = 1.0
     This is just me typing rubbish!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    This is such a journey!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     nXMatrix1 = workPart.NXMatrices.Create(element1)
     
     partImporter1.DestinationCoordinateSystem = nXMatrix1
